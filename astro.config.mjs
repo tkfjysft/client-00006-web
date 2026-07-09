0bcf93ef4c1d72ef3a4e06bbb8587e3f4200a8ea
tkfjysft <tkfjysft.2013.frgn@gmail.com>
2026-07-10 08:18:04 +0900

    fix:CloudFlareへの移行26

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   base: '/single/',
+  build: {
+    assets: '_astro' // デフォルトですが明示します
+  },
 //   outDir: './dist/single/',
   site: 'https://sws.studio-condor.com/',
   vite: {
